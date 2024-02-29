c15066663cf088c381169a4201be3e2b26129003
Nusrat <nusrat.islam@sjinnovation.com>
2024-02-29 16:36:13 +0600

    first

---
+++ index.js
@@ -0,0 +1,29 @@
+const button = document.getElementById("github");
+button.addEventListener("click", function() {
+    window.location.href = "https://github.com/Tanha234/healthcare-realted-website";
+});
+
+const button1 = document.getElementById("live");
+button1.addEventListener("click", function() {
+    window.location.href = "https://helathcare-a604e.web.app/";
+});
+
+const button2 = document.getElementById("online-git");
+button2.addEventListener("click", function() {
+    window.location.href = "https://github.com/Tanha234/online-courses";
+});
+
+const button3 = document.getElementById("onlineLive");
+button3.addEventListener("click", function() {
+    window.location.href = "https://objective-mayer-6ee12f.netlify.app/";
+});
+
+const button4 = document.getElementById("foodgit");
+button4.addEventListener("click", function() {
+    window.location.href = "https://github.com/Tanha234/food-network";
+});
+
+const button5 = document.getElementById("foodlive");
+button5.addEventListener("click", function() {
+    window.location.href = "https://tanha234.github.io/food-network/";
+});
